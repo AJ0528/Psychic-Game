19cbdd54c848ac194832c5923e09c4f8f48ee6f6
Alfredo <puertorocat@gmail.com>
2019-02-28 20:40:12 -0500

    made changes to the js file, added new var's and if/else statements

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,24 @@
-alert("you have 10 guesses. guess 1-10");
 
-for (i=0; i<10; i ++){
-    console.log(i);
+alert("you have 10 guesses");
+
+var Letters = ["a", "b", "c", "d", "e"];
+var Guesesleft = 10;
+var wins = 0;
+var losses=0;
+var userInput=0;
+documemt.onkeyup = function(event){
+var userInput = event.key;
+
+var randonLetter =[Math.floor(Math.random() *Letters.length)];}
+var userInput= prompt ("please pick a letter" + Letters); 
+
+if (userInput != Letters){
+counter -= 1;
+if (counter > Guesesleft){
+    break
+    document.write("no more tries left");
 }
+}
+
+    var 
+
